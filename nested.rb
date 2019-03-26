24cb6dd4fad6c6f6108fc438ae71abf6af8eaaaf
Codey Pinero <codey.pinero@gmail.com>
2019-03-26 16:10:55 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -38,7 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-		 programmer_hash[:alan_kay][:known_for]
+		 return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -66,10 +66,6 @@ def adding_matz
 
 	programmer_hash =
  		{
-				:yukihiro_matsumoto => {
-					:known_for => "Ruby",
-					:languages => ["LISP", "C"]
-				},
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -83,8 +79,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
+		 programmer_hash[:yukihiro_matsumoto] = { :known_for => "Ruby", :languages => ["LISP", "C"] }
 
-
+		 return programmer_hash
 end
 
 def changing_alan
@@ -106,6 +103,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 		 programmer_hash[:alan_kay][:known_for] = alans_new_info
+
 		 return programmer_hash
 end
 
@@ -126,7 +124,7 @@ def adding_to_dennis
         }
      }
 
-		 programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 		 return programmer_hash
 
 end
